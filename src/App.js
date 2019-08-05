9bd22c35e280702065bfc0e96644993afd4647d2
Alex Wang <alexwang177@gmail.com>
2019-08-05 12:18:56 -0500

    added state, bound the getPlayer func to the class, accessed the external api, passed state down as props to components

--- src/App.js
+++ src/App.js
@@ -5,19 +5,66 @@ import Player from './Components/Player.js';
 import Team from './Components/Team.js';
 import Form from './Components/Form.js';
 
-class App extends React.Component{
+const API_KEY = '1c1db10603msh7e97996b2669453p17f294jsna5763e88a5c5';
 
-  getWeather(){
-    
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: undefined,
+      lastName: undefined,
+      position: undefined,
+      team: undefined,
+      conference: undefined,
+      error: undefined
+    }
+    this.getPlayer = this.getPlayer.bind(this);
+  }
+
+  getPlayer(e){
+    e.preventDefault();
+    const searchName = e.target.elements.playerName.value;
+    fetch(`https://free-nba.p.rapidapi.com/players?search=${searchName}`, 
+      {
+        method: 'GET', 
+        headers: {
+          'X-RapidAPI-Key' : API_KEY
+        }
+      }
+    )
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      this.setState({
+        firstName: data.data[0].first_name,
+        lastName: data.data[0].last_name,
+        position: data.data[0].position,
+        team: data.data[0].team.full_name,
+        conference: data.data[0].team.conference,
+        error: ''
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
   
   render(){
     return (
       <div className="App">
         <Title />
-        <Form />
-        <Player />
-        <Team />
+        <Form getPlayer = {this.getPlayer}/>
+        <Player 
+          firstName={this.state.firstName}
+          lastName={this.state.lastName}
+          position={this.state.position}
+          error={this.state.error}
+        />
+        <Team 
+          team={this.state.team}
+          conference={this.state.conference}
+          error={this.state.error}
+        />
       </div>
     );
   }
