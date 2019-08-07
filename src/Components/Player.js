3a161bfc3fe1506c62e991403f65fe17e2a1d56b
Alex Wang <alexwang177@gmail.com>
2019-08-07 09:52:06 -0500

    first and last name same line

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -6,8 +6,7 @@ class Player extends React.Component{
     //let cardStyle = {display:'none'};
     return (
       <div className='card bg-dark'>
-        <p className='text-light'>{this.props.firstName}</p>
-        <p className='text-light'>{this.props.lastName}</p>
+        <p className='text-light'>{this.props.firstName} {this.props.lastName}</p>
         <p className='text-light card-footer'>{this.props.position}</p>
       </div>
     );
