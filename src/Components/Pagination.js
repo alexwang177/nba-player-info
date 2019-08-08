3068009714b92463c8994efe6d2a924f1c045859
Alex Wang <alexwang177@gmail.com>
2019-08-07 19:40:32 -0500

    added top margin to pagination

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -8,7 +8,7 @@ const Pagination = ({postsPerPage, totalPosts, paginate}) => {
     }
 
     return (
-        <nav>
+        <nav className="mt-3">
             <ul className="pagination">
                 {pageNumbers.map(number => (
                     <li key={number} className="page-item">
